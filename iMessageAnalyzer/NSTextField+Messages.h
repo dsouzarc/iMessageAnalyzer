96f46dcbbf5911b299899a6338fcf41e7a0e8d7c
Ryan D'souza <dsouzarc@gmail.com>
2015-11-22 19:02:42 -0500

    Handles when a link is clicked

---
+++ iMessageAnalyzer/NSTextField+Messages.h
@@ -0,0 +1,13 @@
+//
+//  NSTextField+Messages.h
+//  iMessageAnalyzer
+//
+//  Created by Ryan D'souza on 11/22/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSTextField_Messages : NSTextField
+
+@end
---
+++ iMessageAnalyzer/NSTextField+Messages.m
@@ -0,0 +1,30 @@
+//
+//  NSTextField+Messages.m
+//  iMessageAnalyzer
+//
+//  Created by Ryan D'souza on 11/22/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "NSTextField+Messages.h"
+
+@implementation NSTextField_Messages
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+- (void)mouseDown:(NSEvent *)theEvent;
+{
+    NSLog(@"Called");
+    [self sendAction:[self action] to:[self delegate]];
+}
+
+- (void) selectText:(id)sender
+{
+    NSLog(@"Selected...: %@", sender);
+}
+
+@end
