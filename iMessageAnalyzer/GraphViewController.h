ed0a2f4998703d8882633642a5b72e74b299d1a7
Ryan D'souza <dsouzarc@gmail.com>
2015-12-09 00:02:54 -0500

    Began working on a viewcontroller for just graphs

---
+++ iMessageAnalyzer/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  iMessageAnalyzer
+//
+//  Created by Ryan D'souza on 12/9/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GraphViewController : NSViewController
+
+@end
---
+++ iMessageAnalyzer/GraphViewController.m
@@ -0,0 +1,22 @@
+//
+//  GraphViewController.m
+//  iMessageAnalyzer
+//
+//  Created by Ryan D'souza on 12/9/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
