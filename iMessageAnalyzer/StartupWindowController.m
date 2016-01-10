c951cb3699c073abb6270146ce516121ac96e742
Ryan D'souza <dsouzarc@gmail.com>
2016-01-10 04:21:34 -0500

    Methods and alerts and handling for choosing the data source from Messages or which iPhone backup

--- iMessageAnalyzer/StartupWindowController.m
+++ iMessageAnalyzer/StartupWindowController.m
@@ -13,6 +13,9 @@
 @property (strong, nonatomic) MainWindowController *mainWindowController;
 @property (strong, nonatomic) StartupViewController *startupViewController;
 
+@property (strong, nonatomic) NSString *messagesPath;
+@property (strong, nonatomic) NSString *iPhonePath;
+
 @end
 
 @implementation StartupWindowController
@@ -23,6 +26,9 @@
     
     if(self) {
         self.startupViewController = [[StartupViewController alloc] initWithNibName:@"StartupViewController" bundle:[NSBundle mainBundle]];
+        
+        self.messagesPath = [NSString stringWithFormat:@"/Users/%@/Library/Messages", NSUserName()];
+        self.iPhonePath = [NSString stringWithFormat:@"/Users/%@/Library/Application Support/MobileSync/Backup", NSUserName()];
     }
     
     return self;
@@ -52,10 +58,7 @@
 
 - (void) didWishToContinue
 {
-    NSString *messagesPath = [NSString stringWithFormat:@"/Users/%@/Library/Messages", NSUserName()];
-    NSString *iPhonePath = [NSString stringWithFormat:@"/Users/%@/Library/Application Support/MobileSync/Backup", NSUserName()];
-    
-    NSString *description = [NSString stringWithFormat:@"Choose the source from which to analyze your messages:\n\nThe default Mac Messages.app: %@\n\nThe most recent iPhone backup: %@\n", messagesPath, iPhonePath];
+    NSString *description = [NSString stringWithFormat:@"Choose the source from which to analyze your messages:\n\nThe default Mac Messages.app: %@\n\nThe most recent iPhone backup: %@\n", self.messagesPath, self.iPhonePath];
     
     NSAlert *prompt = [[NSAlert alloc] init];
     [prompt setAlertStyle:NSWarningAlertStyle];
@@ -67,10 +70,10 @@
     [prompt beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse response) {
         switch (response) {
             case NSAlertFirstButtonReturn:
-                NSLog(@"MESSAGES");
+                [self messagesDataSource];
                 break;
             case NSAlertSecondButtonReturn:
-                NSLog(@"iPHONE");
+                [self iPhoneDataSource];
                 break;
             default:
                 break;
@@ -78,7 +81,92 @@
     }];
 }
 
+- (void) messagesDataSource
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    NSString *pathForFile = [NSString stringWithFormat:@"%@/chat.db", self.messagesPath];
+    
+    if ([fileManager fileExistsAtPath:pathForFile]){
+        
+        NSString *newFileLocation = [NSString stringWithFormat:@"%@/chat_on_%f.db", self.messagesPath, [[NSDate date] timeIntervalSinceReferenceDate]];
+        
+        NSError *error;
+        [fileManager copyItemAtPath:pathForFile toPath:newFileLocation error:&error];
+        
+        if(error) {
+            [self showErrorPrompt:@"Error making a backup of chat.db" informationText:[NSString stringWithFormat:@"We were not able to make a backup of your Messages.db\n%@", [error description]]];
+        }
+        else {
+            //Good to go
+        }
+        
+    }
+    else {
+        [self showErrorPrompt:@"Error finding chat.db" informationText:[NSString stringWithFormat:@"Error finding Mac's Messages.app chat database at %@. \n\nMaybe Messages.app is not synced with an iCloud account", pathForFile]];
+    }
+}
 
+- (void) showErrorPrompt:(NSString*)messageText informationText:(NSString*)informationText
+{
+    NSAlert *prompt = [[NSAlert alloc] init];
+    [prompt setAlertStyle:NSWarningAlertStyle];
+    [prompt setMessageText:messageText];
+    [prompt setInformativeText:informationText];
+    [prompt addButtonWithTitle:@"Return to main screen"];
+    [prompt beginSheetModalForWindow:self.window completionHandler:nil];
+}
+
+- (void) iPhoneDataSource
+{
+    NSString *fileName = @"3d0d7e5fb2ce288813306e4d4636395e047a3d28";
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error;
+    
+    NSOpenPanel *directoryPanel = [NSOpenPanel openPanel];
+    [directoryPanel setCanChooseDirectories:YES];
+    [directoryPanel setCanChooseFiles:NO];
+    [directoryPanel setCanHide:NO];
+    [directoryPanel setCanCreateDirectories:NO];
+    [directoryPanel setTitle:@"Choose the iPhone backup"];
+    [directoryPanel setMessage:@"Open the directory of the iPhone backup to analyze"];
+    [directoryPanel setDirectoryURL:[NSURL fileURLWithPath:self.iPhonePath]];
+    
+    if([directoryPanel runModal] == NSModalResponseOK) {
+        NSArray *files = [directoryPanel URLs];
+        
+        if(files.count == 0) {
+            [self showErrorPrompt:@"No backups found" informationText:@"No backups found in this directory"];
+            return;
+        }
+        
+        NSString *filePath = [((NSURL*) files[0]) path];
+        if([filePath isEqualToString:self.iPhonePath]) {
+            [self showErrorPrompt:@"No backup was chosen" informationText:@"No backup was chosen"];
+            return;
+        }
+        
+        NSString *iPhoneBackup = [NSString stringWithFormat:@"%@/%@", filePath, fileName];
+        if([fileManager fileExistsAtPath:iPhoneBackup]) {
+            NSString *newFileLocation = [NSString stringWithFormat:@"%@/chat_on_%f.db", self.iPhonePath, [[NSDate date] timeIntervalSinceReferenceDate]];
+            
+            NSError *error;
+            [fileManager copyItemAtPath:iPhoneBackup toPath:newFileLocation error:&error];
+            if(error) {
+                [self showErrorPrompt:@"Error making a backup of iPhone chat" informationText:[NSString stringWithFormat:@"We were not able to make a backup of your Messages.db\n%@", [error description]]];
+            }
+            else {
+                //Good to go
+            }
+        }
+        else {
+            [self showErrorPrompt:@"iPhone backup not found" informationText:@"Either the iPhone's text message backups were not found in this directory or they were encrypted. When syncing or backing up with iTunes, disable encryption"];
+            return;
+        }
+        
+        
+    }
+}
 
 - (void) mainWindow
 {
